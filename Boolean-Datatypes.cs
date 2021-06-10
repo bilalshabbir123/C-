4ab01136745920175bed6b7e97b17e2c977092d4
bilalshabbir123 <85638715+bilalshabbir123@users.noreply.github.com>
2021-06-10 16:24:00 +0500

    Add files via upload

---
+++ Boolean-Datatypes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductiontoCsharp
+{
+    class DataTypes
+    {
+        static void Main(string[] args)
+        {
+            // boolean data type
+            bool b = true;
+            if (b == true)
+                Console.WriteLine("Hi Bilal Shabbir!!");
+            Console.ReadLine();
+        }
+    }
+}
