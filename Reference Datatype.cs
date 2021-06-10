bb2190b0437274eef0a8bd557585981422948934
bilalshabbir123 <85638715+bilalshabbir123@users.noreply.github.com>
2021-06-10 16:28:33 +0500

    Add files via upload

---
+++ Reference Datatype.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductiontoCsharp
+{
+    class DataTypes
+    {
+        static void Main(string[] args)
+        {
+            // declaring string
+            string a = "Bilal";
+
+            //append in a
+            a += " Shabbir";
+            a = a + " Ghulam";
+            Console.WriteLine(a);
+
+            // declare object obj
+            object obj;
+            obj = 20;
+            Console.WriteLine(obj);
+
+            // to show type of object
+            // using GetType()
+            Console.WriteLine(obj.GetType());
+            Console.ReadLine();
+        }
+    }
+}
