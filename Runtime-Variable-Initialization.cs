8b8a465465e27167e1d5a0cd46bb38f5697bdc57
bilalshabbir123 <85638715+bilalshabbir123@users.noreply.github.com>
2021-06-10 16:34:30 +0500

    Add files via upload

---
+++ Runtime-Variable-Initialization.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductiontoCsharp
+{
+    class DataTypes
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter The Integer Value");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Value of Number is:{0}",num);
+            Console.ReadLine();
+        }
+    }
+}
