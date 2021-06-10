22488790724dd458ba627e785d15822af0246080
bilalshabbir123 <85638715+bilalshabbir123@users.noreply.github.com>
2021-06-10 16:40:59 +0500

    Add files via upload

---
+++ Escape sequence.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductiontoCsharp
+{
+    class DataTypes
+    {
+        static void Main(string[] args)
+        {
+            // Escape sequence
+            // \' single quotiion
+            // \"   "charcter
+            // \b  backspace
+            // \a alert or bell
+            // \n new line
+            // \v Vertical Tab
+            string a = "\"BILAL SHABBIR\""; 
+            string b = "\\BILAL SHABBIR\\";
+            string c = "\t Ghulam Shabbir";
+            string d = "One \n Two \n Three ";
+            string e = @"D:\bilal\learning Never Ends\Mosh";
+
+            Console.ReadLine();
+        }
+    }
+}
