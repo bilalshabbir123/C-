7c40560fa9666b3bec818ad466ccec2696fc33ff
bilalshabbir123 <85638715+bilalshabbir123@users.noreply.github.com>
2021-06-11 15:59:39 +0500

    Add files via upload

---
+++ If-else statement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductiontoCsharp
+{
+    class DataTypes
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter a Number:");
+            int Usernumber = int.Parse(Console.ReadLine());
+            if (Usernumber==10)
+            {
+                Console.WriteLine("Your Number is 10");
+            }
+            else if (Usernumber==20)
+            {
+                Console.WriteLine("Your Number is 20");
+            }
+            else if (Usernumber==30)
+            {
+                Console.WriteLine("Your Number is 30");
+            }
+            else
+            {
+                Console.WriteLine("Your Number is Not 10 20 and 30");
+            }
+            Console.ReadLine();
+        }
+    }
+}
