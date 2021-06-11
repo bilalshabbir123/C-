e8e64385393eddb435dc8f3e97c70450c37bec90
bilalshabbir123 <85638715+bilalshabbir123@users.noreply.github.com>
2021-06-11 05:50:16 +0500

    Add files via upload

---
+++ One-Dimensional-array.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductiontoCsharp
+{
+    class DataTypes
+    {
+        static void Main(string[] args)
+        {
+            //declares a 1D Array of string
+            string[] Weekdays;
+            //allocating memory for days
+            Weekdays=new string[]{ "Monday","Thuesday","Wednesday","Thursday","Friday","Saturday","Sunday" };
+            //displaying the Elements of Array
+            foreach (string day in Weekdays)
+            {
+                Console.WriteLine(day);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
