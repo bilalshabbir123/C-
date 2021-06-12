75f737434c606cc964b454619c1b564edb957a60
bilalshabbir123 <85638715+bilalshabbir123@users.noreply.github.com>
2021-06-12 08:29:28 +0500

    Add files via upload

---
+++ Method Value Paarameter out keyword.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductiontoCsharp
+{
+    class DataTypes
+    {
+        public static void Methods(int i,int j,out int add,out int subtract,out int multiply,out int division,out int remainder)
+            {
+            add = i + j;
+            subtract = i + j;
+            multiply = i - j;
+            division = i * j;
+            remainder = i / j;
+            }
+        static void Main(string[] args)
+        {
+            int ad, sub, mul, div, rem = 0;
+            Methods(20,10,out ad,out sub,out mul,out div,out rem);
+            Console.WriteLine("Addition={0}\nSubtraction={1}\nMultiplication={2}\nDivision={3}\nRemainder={4}",ad,sub,mul,div,rem);
+            Console.WriteLine("Addition={0}"+", "+"Subtraction={1}"+", "+"Multiplication={2}"+", "+"Division={3}"+"and "+"Remainder={4}",ad,sub,mul,div,rem);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Method Value Paarameter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductiontoCsharp
+{
+    class DataTypes
+    {
+        public static void Simplemethod(ref int j)
+        {
+            j = 101;
+        }
+        static void Main(string[] args)
+        {
+            int i = 0;
+            Simplemethod(ref i);
+            Console.WriteLine(i);
+            Console.ReadLine();
+        }
+    }
+}
